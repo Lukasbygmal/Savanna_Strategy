270724ea66b8892306367b998aac0a5e8f546003
lukasbygmal <lukasbygmal@gmail.com>
2024-11-04 22:09:06 +0100

    possible moves for all done, with working tests

--- logic.py
+++ logic.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Literal
 
 
-Color = Literal['red', 'blue']
+Color = Literal['red', 'blue'] #blue down, red up
 
 RED = "\033[31m" 
 BLUE = "\033[34m" 
@@ -10,12 +10,13 @@ RESET = "\033[0m"
 
 
 class Game:
+    
     def __init__(self):
         pass
 
 class Board:
     def __init__(self):
-        self.grid = [[' ' for _ in range(8)] for _ in range(8)]
+        self.grid = [[None for _ in range(8)] for _ in range(8)]
 
     def display(self):
         print("  A B C D E F G H")
@@ -26,10 +27,53 @@ class Board:
     def setup(self):
         pass
 
+    def pos_is_empty(self,position)-> bool:
+        if (self.grid[position[0]][position[1]]==None):
+            return True
+        else:
+            False
+
+    def place_has_piece_of_color(self,position,color)-> bool:
+        if (self.pos_is_empty(position)==False):
+            piece = self.get_piece_at_pos()
+            if(piece.get_color()==color):
+                return True
+        return False
+
+    def place_piece(self,Piece,position):
+        assert(self.pos_is_empty(position))
+        self.grid[position[0]][position[1]]=Piece
+
+    def get_piece_at_pos(self,position):
+        return self.grid[position[0]][position[1]]
+
+    def pos_inside_board(self,position)-> bool:
+        return 0 <= position[0] < 8 and 0 <= position[1] < 8
+    
+    def add_eligble_move(self,new_pos,moves,own_color) ->bool:
+        result = False
+        if (self.pos_inside_board(new_pos)):
+                if (self.pos_is_empty(new_pos)):
+                    moves.append(new_pos)
+                    result = True
+
+                else:
+                    piece = self.get_piece_at_pos(new_pos)
+                    if(piece.get_color() != own_color):
+                        moves.append(new_pos)
+                        result = True
+        return result
+        
+
+
+
 class Piece:
+    piece_type = None
     def __init__(self,color: Color,position: tuple):
         self.__color = color
         self.__position = position
+        if self.piece_type is None:
+            raise NotImplementedError("Subclasses must define 'piece_type'.")
 
 
     @abstractmethod
@@ -42,8 +86,15 @@ class Piece:
     def get_position(self):
         return self.__position
     
-    def move(self,new_position):
+    def move(self,new_position,board):
         self.__position = new_position
+
+    def piece_type(self):
+        return self.piece_type
+    
+    def be_taken(self,board):
+        board.grid[self.position] = (0,0)
+
     
     @abstractmethod
     def __str__(self) -> str:
@@ -51,12 +102,29 @@ class Piece:
 
 
 
-class Ape(Piece):
+class Ape(Piece): ##TODO checck if actually cheks bounds
+    piece_type = "ape"
     def __init__(self, color, initial_position):
         super().__init__(color,initial_position)
 
-    def get_possible_moves(self,position, board):
-        pass
+    def get_possible_moves(self,position, board)->list:
+        moves = []
+        direction = 1
+        if (self.get_color()=='blue'):
+            direction = -1
+        for i in range(1,3):
+            new_pos = (position[0]+direction*i,position[1])
+            if (board.pos_inside_board(new_pos)):
+                if (board.pos_is_empty):
+                    moves.append(new_pos)
+                else:
+                    piece = board.get_piece_at_pos(new_pos)
+                    if(piece.color == self.get_color() or piece.piece_type == "ape"):
+                        ()
+                    else:
+                        moves.append(new_pos)
+            
+        return moves
 
     def __str__(self) -> str:
         if (self.get_color()== 'red'):
@@ -66,11 +134,37 @@ class Ape(Piece):
 
 
 class Snake(Piece):
+    piece_type = "snake"
     def __init__(self, color, initial_position):
         super().__init__(color,initial_position)
     
-    def get_possible_moves(self,position, board):
-        pass
+    def get_possible_moves(self,position, board)->list:
+        moves = []
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        for d in directions:
+            l_path = True
+            r_path = True
+            for x in range (1,6):
+                if (x%2 == 0):
+                    new_pos = (position[0] + d[0]*x,position[1]+ d[1]*x)
+                    if (board.add_eligble_move(new_pos,moves,self.get_color())==False):
+                        break
+
+                else:
+                    
+                    if l_path:
+                        l_pos = (position[0] + x * d[0] + d[1], position[1] + x * d[1] + d[0])
+                        if (board.add_eligble_move(l_pos,moves,self.get_color())==False):
+                            l_path = False
+                
+                    if r_path:
+                        r_pos = (position[0] + x * d[0] - d[1], position[1] + x * d[1] - d[0])
+                        if (board.add_eligble_move(r_pos,moves,self.get_color())==False):
+                            r_path = False
+        moves = set(moves)
+        return moves
+        
+
             
 
     def __str__(self) -> str:
@@ -81,11 +175,31 @@ class Snake(Piece):
 
 
 class Crab(Piece):
+    piece_type = "crab"
     def __init__(self, color, initial_position):
         super().__init__(color,initial_position)
     
     def get_possible_moves(self,position, board):
-        pass
+        moves = []
+        for i in range(-1,2):
+            new_pos = (position + (i,0))
+            new_pos = (position[0]+ i, position[1])
+            board.add_eligble_move(new_pos,moves,self.get_color())
+        l_path = True
+        r_path = True
+        for i in range(1,8):
+            l_pos = (position[0], position[1]+i)
+            r_pos = (position[0], position[1]-i)
+            if (board.add_eligble_move(l_pos,moves,self.get_color())==False):
+                l_pos = False
+
+            if (board.add_eligble_move(r_pos,moves,self.get_color())==False):
+                r_pos = False
+
+            if (l_path == False and r_path == False):
+                break
+
+        return moves
             
 
     def __str__(self) -> str:
@@ -95,11 +209,19 @@ class Crab(Piece):
             return(f"{BLUE} C {RESET}")
 
 class Meerkat(Piece):
+    piece_type = "meerkat"
     def __init__(self, color, initial_position):
         super().__init__(color,initial_position)
     
     def get_possible_moves(self,position, board):
-        pass
+        moves = []
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        for d in directions:
+            for i in range(1,3):
+                new_pos = (position[0] + i *d[0], position[1] + i *d[1])
+                board.add_eligble_move(new_pos,moves,self.get_color())
+                
+        return moves
             
 
     def __str__(self) -> str:
@@ -109,11 +231,24 @@ class Meerkat(Piece):
             return(f"{BLUE} M {RESET}")
 
 class Lynx(Piece):
+    piece_type = "lynx"
     def __init__(self, color, initial_position):
         super().__init__(color,initial_position)
     
     def get_possible_moves(self,position, board):
-        pass
+        moves = []
+        directions = [(1,1),(-1,1),(1,-1),(-1,-1)]
+        for d in directions:
+            for i in range(1,8):
+                new_pos = (position[0] + d[0]*i,position[1] + d[1]*i)
+                board.add_eligble_move(new_pos,moves,self.get_color())
+
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        for d in directions:
+            new_pos = position + d
+            new_pos = (position[0] + d[0],position[1] + d[1])
+            board.add_eligble_move(new_pos,moves,self.get_color())
+        return moves
             
 
     def __str__(self) -> str:
--- test_suite.py
+++ test_suite.py
@@ -1,37 +1,92 @@
 import unittest
-from logic import Board, Ape, Color
+from logic import Board, Ape, Snake, Crab, Meerkat, Lynx
 
 class TestBoard(unittest.TestCase):
     def setUp(self):
         self.board = Board()
 
-    def test_initial_board(self):
-        self.assertEqual(len(self.board.grid), 8)
-        self.assertTrue(all(len(row) == 8 for row in self.board.grid))
-        self.assertTrue(all(cell == ' ' for row in self.board.grid for cell in row))
+    def test_initial_board_empty(self):
+        for row in self.board.grid:
+            for cell in row:
+                self.assertEqual(cell, None)
 
-    def test_display(self):
-        try:
-            self.board.display()
-        except Exception:
-            self.fail("Display method raised an exception")
+    def test_pos_is_empty(self):
+        position = (3, 3)
+        self.assertTrue(self.board.pos_is_empty(position))
+        
+    def test_place_piece(self):
+        ape = Ape("red", (3, 3))
+        self.board.place_piece(ape, (3, 3))
+        self.assertEqual(self.board.get_piece_at_pos((3, 3)), ape)
+        self.assertFalse(self.board.pos_is_empty((3, 3)))
 
-class TestPiece(unittest.TestCase):
-    def test_Ape_initialization(self):
-        Ape_piece = Ape('red', (0, 0))
-        self.assertEqual(Ape_piece.get_color(), 'red')
-        self.assertEqual(Ape_piece.get_position(), (0, 0))
+    def test_pos_inside_board(self):
+        self.assertTrue(self.board.pos_inside_board((0, 0)))
+        self.assertTrue(self.board.pos_inside_board((7, 7)))
+        self.assertFalse(self.board.pos_inside_board((8, 8)))
+        self.assertFalse(self.board.pos_inside_board((-1, 0)))
 
-    def test_Ape_move(self):
-        Ape_piece = Ape('blue', (0, 0))
-        Ape_piece.move((1, 1))
-        self.assertEqual(Ape_piece.get_position(), (1, 1))
+class TestApe(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+        self.ape = Ape("red", (3, 3))
+        self.board.place_piece(self.ape, (3, 3))
 
-    def test_Ape_string_representation(self):
-        red_Ape = Ape('red', (0, 0))
-        blue_Ape = Ape('blue', (0, 0))
-        self.assertIn('A', str(red_Ape)) 
-        self.assertIn('A', str(blue_Ape))
+    def test_ape_possible_moves(self):
+        moves = self.ape.get_possible_moves(self.ape.get_position(), self.board)
+        expected_moves = [(4, 3), (5, 3)]
+        self.assertEqual(sorted(moves), sorted(expected_moves))
 
-if __name__ == '__main__':
+class TestSnake(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+        self.snake = Snake("blue", (4, 4))
+        self.board.place_piece(self.snake, (4, 4))
+
+    def test_snake_possible_moves(self):
+        moves = self.snake.get_possible_moves(self.snake.get_position(), self.board)
+        expected_moves = [(5, 5), (3, 3), (3, 5), (5, 3), (6, 4), (2, 4), (4, 6), (4, 2), 
+                          (7, 5), (7, 3), (1, 5), (1, 3), (5, 7), (3, 7), (5, 1), (3, 1),
+                          (4, 0), (0, 4)]
+        self.assertEqual(sorted(moves), sorted(expected_moves))
+
+class TestCrab(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+        self.crab = Crab("red", (4, 4))
+        self.board.place_piece(self.crab, (4, 4))
+
+    def test_crab_possible_moves(self):
+        moves = self.crab.get_possible_moves(self.crab.get_position(), self.board)
+        expected_moves = [(3, 4), (5, 4), (4,3), (4,2), (4,1), (4,0), (4,5), (4,6), (4,7)]
+        self.assertEqual(sorted(moves), sorted(expected_moves))
+
+class TestMeerkat(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+        self.meerkat = Meerkat("blue", (3, 3))
+        self.board.place_piece(self.meerkat, (3, 3))
+
+    def test_meerkat_possible_moves(self):
+        moves = self.meerkat.get_possible_moves(self.meerkat.get_position(), self.board)
+        expected_moves = [(4, 3), (5, 3), (2, 3), (1, 3), (3, 4), (3, 5), (3, 2), (3, 1)]
+        self.assertEqual(sorted(moves), sorted(expected_moves))
+
+class TestLynx(unittest.TestCase):
+    def setUp(self):
+        self.board = Board()
+        self.lynx = Lynx("red", (3, 3))
+        self.board.place_piece(self.lynx, (3, 3))
+
+    def test_lynx_possible_moves(self):
+        moves = self.lynx.get_possible_moves(self.lynx.get_position(), self.board)
+        expected_moves = [
+            (4, 3), (2, 3), (3, 4), (3, 2), (4, 4), (5, 5), (6, 6), (7, 7),
+            (2, 2), (1, 1), (0, 0), (2, 4), (1, 5), (0, 6), (4, 2), (5, 1),
+            (6, 0),               
+        ]
+        self.assertEqual(sorted(moves), sorted(expected_moves))
+
+
+if __name__ == "__main__":
     unittest.main()
